de174ab13c8a71ba3731a21f7c6f0fe97552839d
timazarenok <timofey.zarenok@gmail.com>
2020-10-20 17:36:12 +0300

    refactor grep

--- ruby/grep/grep.rb
+++ ruby/grep/grep.rb
@@ -6,11 +6,70 @@
 # To get started with TDD, see the `README.md` file in your
 # `ruby/grep` directory.
 
-require_relative('flags.rb')
+require 'io/console'
 
-class Grep < Flags
+class Grep
+  def initialize(pattern, flags, files)
+    @pattern = pattern
+    @flags = flags
+    @files = files
+  end
+
   def self.grep(pattern, flags, files)
-    result = Flags.new(flags, files).grep(pattern).join.strip
+    result = Grep.new(pattern, flags, files).grep
+  end
+
+  def grep
+    create_pattern
+    f
+    @flags.include?('-l') && @result.length > 1 ? @result.uniq : @result
+  end
+
+  private
+
+  def f
+    combine_files.map { |f_name, lines| check_flags(f_name, lines) }
+  end
+
+  def combine_files
+    @files.each_with_object({}) do |f_name, lines|
+      lines[f_name] = get_lines(f_name)
+    end
+  end
+
+  def get_lines(f_name)
+    file = File.open(f_name)
+    file.readlines.map(&:chomp)
+    file.close
+  end
+
+  def check_flags(file, lines)
+    return l_flag(file, lines) if @flags.include?('-l')
+    lines.each_with_index do |line, index|
+      "#{multi_files(file)}#{need_index(index)}#{line}" if v_match(line)
+    end
+  end
+
+  def create_pattern
+    i = @flags.include?('-i') ? Regexp::IGNORECASE : false
+    x = @flags.include?('-x') ? "^#{@pattern}$" : @pattren
+    @pattern = Regexp.new(x, i)
+  end
+
+  def need_index(index)
+    @flags.include?('-n') ? "#{index + 1}:" : ''
+  end
+
+  def multi_files(file)
+    @files.size > 1 ? "#{file}:" : ''
+  end
+
+  def v_match(string)
+    @flags.include?('-v') ? !string.match(@pattern) : string.match(@pattern)
+  end
+
+  def l_flag(file, lines)
+    lines.any? { |line| v_match(line) } ? file : ''
   end
 end
 
